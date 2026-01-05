8910c3c2f8f738546d7507ce21b140703e301b54
Riya Mehta <riyamehtaatwork@gmail.com>
2026-01-05 11:59:01 +0530

    assignment 1 test resolved

--- src/User.repository.ts
+++ src/User.repository.ts
@@ -1,4 +1,11 @@
-import Database, { UserRequestDto } from './Database'
+import Database, { IUser, UserRequestDto } from './Database'
 
 // NOTE: Make db public instead of private. This is required for the tests.
-export class UserRepository {}
+export class UserRepository {
+  public readonly db = Database
+
+  async createUser(user: Omit<IUser, 'id'>) {
+    const createdUser = await this.db.create(user)
+    return createdUser
+  }
+}
