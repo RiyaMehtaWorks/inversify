7adb706de0c7ffe85e0d045d8644913a7f793027
Riya Mehta <riyamehtaatwork@gmail.com>
2026-01-05 15:35:49 +0530

    assignment 2 boostrap.test ☑️

--- src/User.repository.ts
+++ src/User.repository.ts
@@ -1,11 +1,11 @@
-import Database, { IUser, UserRequestDto } from './Database'
+import Database, { UserRequestDto } from './Database'
 
 // NOTE: Make db public instead of private. This is required for the tests.
 export class UserRepository {
   public readonly db = Database
 
-  async createUser(user: Omit<IUser, 'id'>) {
-    const createdUser = await this.db.create(user)
+  async createUser(userData: UserRequestDto) {
+    const createdUser = await this.db.create(userData)
     return createdUser
   }
 }
--- src/User.service.ts
+++ src/User.service.ts
@@ -2,4 +2,11 @@ import { UserRequestDto } from './Database'
 import { UserRepository } from './User.repository'
 
 // NOTE: Make userRepo public and not private. This is required for the tests!
-export class UserService {}
+export class UserService {
+  constructor(public readonly userRepo: UserRepository) {}
+
+  async createUser(userData: UserRequestDto) {
+    const createdUser = this.userRepo.createUser(userData)
+    return createdUser
+  }
+}
